59ea0cba8a7650264f19f3fb318ebe1d2a72f00c
Bruno Lucio <bruno.lucio@sonner.com.br>
2020-12-02 08:34:28 -0300

    first commit

--- Desafio back/src/main/java/br/com/sonner/desafio/WebConfig.java
+++ Desafio back/src/main/java/br/com/sonner/desafio/WebConfig.java
@@ -1,2 +1,18 @@
-package br.com.sonner.desafio;public class WebConfig {
+package br.com.sonner.desafio;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+    @Configuration
+    @EnableWebMvc
+    public static class WebConfiguracao implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");// <- assim permite de qualquer origem, troque "/**" pelo seu dominio por exemplo "http://meudominio.com"
+        }
+    }
 }
