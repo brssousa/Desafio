8735ed1e3e393c5add1309e0c8eec2e72c31f89c
eliseu <eliseu@sonner.com.br>
2020-12-04 10:23:37 -0300

    Modelos no front

---
+++ desafio-front/src/app/model/itens.ts
@@ -0,0 +1,12 @@
+import {NotaFiscal} from "./nota.fiscal";
+
+export class Itens {
+    id:number;
+
+  nota: NotaFiscal;
+
+  item: string;
+
+  valor: number;
+
+}
---
+++ desafio-front/src/app/model/nota.fiscal.ts
@@ -0,0 +1,15 @@
+import {Itens} from "./itens";
+
+export class NotaFiscal {
+    id:number;
+
+  numero: number;
+
+  fornecedor: string;
+
+  data: Date;
+
+  itens: Itens[];
+
+
+}
